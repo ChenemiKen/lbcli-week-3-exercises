625cff615bf6e3463c74b279eae078365d94521b
ChenemiKen <chenemiken15@gmail.com>
2025-04-27 12:39:37 +0100

    solutins

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # Create a native segwit address and get the public key from the address.
+address=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress -addresstype bech32)
+bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo $address | jq -r '.pubkey'
